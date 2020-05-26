e9301c7dd0131c71da546c05feb1093793d73688
Ngocwalle <34917159+Ngocwalle@users.noreply.github.com>
2020-05-26 07:22:47 +0700

    test

--- admincp/modules/config.php
+++ admincp/modules/config.php
@@ -1,8 +1,8 @@
 <?php
 	$tenmaychu='localhost';
 	$tentaikhoan='root';
-	$pass='';
-	$csdl='nhom 19';
+	$pass='ngoc123';
+	$csdl='nhom19';
 	$conn=mysqli_connect($tenmaychu,$tentaikhoan,$pass,$csdl) or die('Không kết nối được');
 	mysqli_select_db($conn,$csdl);
 	mysqli_query ($conn,"SET NAMES 'UTF8'");
